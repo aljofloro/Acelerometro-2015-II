5855c9fb6d089fa29522f65c87e94248acd99073
aljofloro <aljofloro@gmail.com>
2015-11-27 23:21:18 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/upt/acelerometro/MainActivity.java
@@ -0,0 +1,117 @@
+package pe.edu.upt.acelerometro;
+
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+
+    private long last_update = 0, last_movement = 0;
+    private float prevX = 0, prevY = 0, prevZ = 0;
+    private float curX = 0, curY = 0, curZ = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        synchronized (this) {
+            long current_time = event.timestamp;
+
+            curX = event.values[0];
+            curY = event.values[1];
+            curZ = event.values[2];
+
+            if (prevX == 0 && prevY == 0 && prevZ == 0) {
+                last_update = current_time;
+                last_movement = current_time;
+                prevX = curX;
+                prevY = curY;
+                prevZ = curZ;
+            }
+
+            long time_difference = current_time - last_update;
+            if (time_difference > 0) {
+                float movement = Math.abs((curX + curY + curZ) - (prevX - prevY - prevZ)) / time_difference;
+                int limit = 1500;
+                float min_movement = 1E-6f;
+                if (movement > min_movement) {
+                    if (current_time - last_movement >= limit) {
+                        Toast.makeText(getApplicationContext(), "Hay movimiento de " + movement, Toast.LENGTH_SHORT).show();
+                    }
+                    last_movement = current_time;
+                }
+                prevX = curX;
+                prevY = curY;
+                prevZ = curZ;
+                last_update = current_time;
+            }
+
+
+            ((TextView) findViewById(R.id.txtAccX)).setText("Acelerómetro X: " + curX);
+            ((TextView) findViewById(R.id.txtAccY)).setText("Acelerómetro Y: " + curY);
+            ((TextView) findViewById(R.id.txtAccZ)).setText("Acelerómetro Z: " + curZ);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if (sensors.size() > 0) {
+            sm.registerListener(this, sensors.get(0), SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sm.unregisterListener(this);
+        super.onStop();
+    }
+}
